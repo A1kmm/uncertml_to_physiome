1296bfc79ad298b0ae87618eee03223113e98956
Andrew Miller <ak.miller@auckland.ac.nz>
2011-12-06 16:51:14 +1300

    Implement more of the UncertML -> MathML translation

--- UncertML2CellML.hs
+++ UncertML2CellML.hs
@@ -8,7 +8,7 @@ import Text.Parsec.Language
 
 data UncertMLDistribution =
   AsSamples [[Double]] |
-  DirichletDistribution { dirichletConcentration :: Double } |
+  DirichletDistribution { dirichletConcentration :: [Double] } |
   ExponentialDistribution { exponentialRate :: Double } |
   GammaDistribution { gammaShape :: Double, 
                       gammaScale :: Double } |
@@ -33,7 +33,7 @@ data UncertMLDistribution =
                          studentTDegF :: Int } |
   UniformDistribution { uniformMinimum :: Double,
                         uniformMaximum :: Double } |
-  MixtureModel { mixtureComponent :: [UncertMLDistribution] } |
+  MixtureModel { mixtureComponent :: [(Double, UncertMLDistribution)] } |
   MultivariateNormalDistribution { mvnormMean :: [Double],
                                    mvnormCov :: [[Double]] } |
   MultivariateStudentTDistribution { mvtMean :: [Double],
@@ -55,8 +55,8 @@ data UncertMLDistribution =
   WishartDistribution { wishartDegF :: Double, wishartScale :: [[Double]] } |
   BernoulliDistribution { bernoulliProb :: Double } deriving(Eq, Ord, Show)
 
-data MathML2Expression = M2Apply MathML2Op [MathML2Expression] | M2Ci String | M2Cn String Double | M2Vector [MathML2Expression] | M2True |
-                         M2False | M2Pi | M2EulerGamma | M2ExponentialE | M2Piecewise [(MathML2Expression, MathML2Expression)] deriving (Eq, Ord, Show)
+data MathML2Expression = M2Apply MathML2Op [MathML2Expression] | M2Ci String | M2Cn String Double | M2Lambda String MathML2Expression | M2Vector [MathML2Expression] | M2True |
+                         M2False | M2Infinity | M2Pi | M2EulerGamma | M2ExponentialE | M2Piecewise [(MathML2Expression, MathML2Expression)] (Maybe MathML2Expression) deriving (Eq, Ord, Show)
 data MathML2Op = M2Quotient | M2Factorial | M2Divide | M2Max | M2Min | M2Minus | M2Plus | M2Power | M2Rem | M2Times | M2Root { m2rootDegree :: Maybe MathML2Expression } | M2Gcd | M2And | M2Or | M2Xor | M2Not | M2Implies | M2Abs | M2Lcm | M2Floor | M2Ceiling | M2Eq | M2Neq | M2Gt | M2Lt | M2Geq | M2Leq | M2Factorof | M2Int { m2intLowlimit :: Maybe MathML2Expression, m2intUplimit :: Maybe MathML2Expression, m2intDegree :: Maybe MathML2Expression, m2intBvar :: String } | M2Diff { m2intBvar :: String } | M2Exp | M2Ln | M2Log { m2logLogbase :: Maybe MathML2Expression } | M2Csymbol String  deriving (Eq, Ord, Show)
 
 liftArrow :: Arrow a => (c -> d) -> a b c -> a b d
@@ -97,7 +97,7 @@ xmlToUncertMLDistribution =
    liftArrow AsSamples (listA $ getChildren >>> hasQName (mkNsName "un:Realisation" uncertmlNS) />
                                 hasQName (mkNsName "un:values" uncertmlNS) >>> parseCombinedChildText uncertmlListOfFloat)) <+>
   (hasQName (mkNsName "un:DirichletDistribution" uncertmlNS) >>>
-   liftArrow DirichletDistribution (getChildren >>> hasQName (mkNsName "un:concentration" uncertmlNS) >>> readCombinedChildText)) <+>
+   liftArrow DirichletDistribution (getChildren >>> hasQName (mkNsName "un:concentration" uncertmlNS) >>> parseCombinedChildText uncertmlListOfFloat)) <+>
   (hasQName (mkNsName "un:ExponentialDistribution" uncertmlNS) >>>
    liftArrow ExponentialDistribution (getChildren >>> hasQName (mkNsName "un:rate" uncertmlNS) >>> readCombinedChildText)) <+>
   (hasQName (mkNsName "un:GammaDistribution" uncertmlNS) >>>
@@ -150,7 +150,7 @@ xmlToUncertMLDistribution =
   ) <+>
   (hasQName (mkNsName "un:MixtureModel" uncertmlNS) >>>
    liftArrow MixtureModel
-     (listA (getChildren >>> hasQName (mkNsName "un:component" uncertmlNS) /> xmlToUncertMLDistribution))
+     (listA (getChildren >>> hasQName (mkNsName "un:component" uncertmlNS) >>> ((getAttrValue "weight" >>^ read) &&& (getChildren >>> xmlToUncertMLDistribution))))
   ) <+>
   (hasQName (mkNsName "un:MultivariateNormalDistribution" uncertmlNS) >>>
    liftArrow2 MultivariateNormalDistribution
@@ -244,57 +244,228 @@ main = do
 doConversion :: String -> String
 doConversion = concat . runLA (xread >>> propagateNamespaces >>> (xshow this <+> constA "Out: " <+> (xmlToUncertMLDistribution >>^ show)))
 
+unIsDiscrete (PoissonDistribution {}) = True
+unIsDiscrete _ = False
+
 unToMMLAST (AsSamples v) = M2Apply (M2Csymbol "http://www.cellml.org/uncertainty-1#distributionFromRealisations")
                              [M2Vector (map (\r -> M2Vector (map (M2Cn "dimensionless") r)) v)]
-{-
-  DirichletDistribution { dirichletConcentration :: Double } |
-  ExponentialDistribution { exponentialRate :: Double } |
-  GammaDistribution { gammaShape :: Double, 
-                      gammaScale :: Double } |
-  InverseGammaDistribution { invGammaShape :: Double, 
-                             invGammaScale :: Double } |
-  NormalInverseGammaDistribution {
-    normalInvGammaMean :: Double,
-    normalInvGammaVarianceScaling :: Double,
-    normalInvGammaShape :: Double,
-    normalInvGammaScale :: Double } |
-  PoissonDistribution { poissonRate :: Double } |
-  NormalDistribution { normalMean :: Double, 
-                       normalVariance :: Double } |
-  BinomialDistribution { binomialNumTrials :: Int,
-                         binomialPSuccess :: Double } |
-  MultinomialDistribution { multinomNumTrials :: Int,
-                            multinomProbabilities :: [Double]} |
-  LogNormalDistribution { logNormalLogScale :: Double,
-                          logNormalShape :: Double } |
-  StudentTDistribution { studentTLocation :: Double,
-                         studentTScale :: Double,
-                         studentTDegF :: Int } |
-  UniformDistribution { uniformMinimum :: Double,
-                        uniformMaximum :: Double } |
-  MixtureModel { mixtureComponent :: [UncertMLDistribution] } |
-  MultivariateNormalDistribution { mvnormMean :: [Double],
-                                   mvnormCov :: [[Double]] } |
-  MultivariateStudentTDistribution { mvtMean :: [Double],
-                                     mvtCov :: [[Double]],
-                                     mvtDegF :: Int } |
-  BetaDistribution { betaAlpha :: Double, betaBeta :: Double } |
-  LaplaceDistribution { laplaceAlpha :: Double, laplaceBeta :: Double } |
-  CauchyDistribution { cauchyLocation :: Double, cauchyScale :: Double } |
-  WeibullDistribution { weibullLocation :: Double, weibullScale :: Double } |
-  LogisticDistribution { logisticLocation :: Double, logisticScale :: Double } |
-  ChiSquareDistribution { chiSqDegF :: Int } |
-  GeometricDistribution { geoProbability :: Double } |
-  HypergeometricDistribution { hypergeoNumSuccess :: Int,
-                               hypergeoNumTrials :: Int,
-                               hypergeoPopSize :: Int } |
-  FDistribution { fdistDenominator :: Double, fdistNumerator :: Double } |
-  NegativeBinomialDistribution { negbinNumFailures :: Int, negbinProb :: Double } |
-  ParetoDistribution { paretoScale :: Double, paretoShape :: Double } |
-  WishartDistribution { wishartDegF :: Double, wishartScale :: [[Double]] } |
-  BernoulliDistribution { bernoulliProb :: Double } deriving(Eq, Ord, Show)
--}
+unToMMLAST (DirichletDistribution conc) = error "Multivariate dirichlet distrib generator - todo"
 
+unToMMLAST v | unIsDiscrete v = M2Apply (M2Csymbol "http://www.cellml.org/uncertainty-1#distributionFromMass") [M2Lambda "massBvar" $ unToMMLASTPMF (M2Ci "massBvar") v]
+unToMMLAST v = M2Apply (M2Csymbol "http://www.cellml.org/uncertainty-1#distributionFromDensity") [M2Lambda "densityBvar" $ unToMMLASTPDF (M2Ci "densityBvar") v]
+
+unToMMLASTPDF x (ExponentialDistribution rate) =
+  M2Piecewise [(M2Cn "dimensionless" 0, M2Apply M2Lt [x, (M2Cn "dimensionless" 0)])] $ Just $
+    M2Apply M2Times [M2Cn "dimensionless" rate,
+                     M2Apply M2Exp [M2Apply M2Minus [M2Apply M2Times [M2Cn "dimensionless" rate, x]]]]
+unToMMLASTPDF x (GammaDistribution shape scale) =
+  let
+    shapeP = M2Cn "dimensionless" shape
+    scaleP = M2Cn "dimensionless" scale
+  in
+   M2Apply M2Times [M2Apply M2Power [x, M2Apply M2Minus [M2Apply M2Minus [shapeP, M2Cn "dimensionless" 1]]],
+                    M2Apply M2Divide [M2Apply M2Exp [M2Apply M2Minus [M2Apply M2Divide [x, scaleP]]],
+                                      M2Apply M2Times [M2Apply M2Power [scaleP, shapeP], mmlGammaFunc shapeP]]]
+  
+unToMMLASTPDF x (InverseGammaDistribution shape scale) =
+  let
+    alpha = M2Cn "dimensionless" shape
+    beta = M2Cn "dimensionless" scale
+  in
+   M2Apply M2Times [
+     M2Apply M2Divide [M2Apply M2Power [beta, alpha], mmlGammaFunc alpha],
+     M2Apply M2Power [x, M2Apply M2Minus [M2Apply M2Minus [alpha],
+                                          M2Cn "dimensionless" 1]],
+     M2Apply M2Exp [M2Apply M2Minus [M2Apply M2Divide [beta, x]]]
+     ]
+
+unToMMLASTPDF x (NormalInverseGammaDistribution mean varScal shape scale) = error "Normal Inverse Gamma Distribution is not yet supported"
+
+unToMMLASTPDF x (NormalDistribution mean var) =
+  let
+    mu = M2Cn "dimensionless" mean
+    sigma2 = M2Cn "dimensionless" var
+  in
+   M2Apply M2Times [
+     M2Apply M2Divide [M2Cn "dimensionless" 1,
+                       M2Apply (M2Root Nothing) [
+                         M2Apply M2Times [M2Cn "dimensionless" 2,
+                                          M2Pi, sigma2]]
+                      ],
+     M2Apply M2Exp [M2Apply M2Minus [
+                       M2Apply M2Divide [
+                          M2Apply M2Power [M2Apply M2Minus [x, mu], M2Cn "dimensionless" 2],
+                          M2Apply M2Times [M2Cn "dimensionless" 2, sigma2]
+                                        ]
+                                    ]
+                   ]
+     ]
+
+unToMMLASTPDF x (MultinomialDistribution numTrials pSuccesses) = error "Multinomial isn't supported yet"
+
+unToMMLASTPDF x (LogNormalDistribution logScale shape) =
+  let
+    mu = M2Cn "dimensionless" logScale
+    sigma = M2Cn "dimensionless" shape
+  in
+   M2Piecewise [(M2Cn "dimensionless" 0, M2Apply M2Leq [x, M2Cn "dimensionless" 0])] $ Just $
+   M2Apply M2Times [
+       M2Apply M2Divide [
+          M2Cn "dimensionless" 1,
+          M2Apply M2Times [
+            x, sigma, M2Apply (M2Root Nothing) [
+               M2Cn "dimensionless "2,
+               M2Pi
+                 ]
+            ]
+       ],
+       M2Apply M2Exp [
+           M2Apply M2Minus [
+              M2Apply M2Divide [
+                 M2Apply M2Power [
+                    M2Apply M2Minus [M2Apply M2Ln [x], mu],
+                    M2Cn "dimensionless" 2
+                   ],
+                 M2Apply M2Times [
+                     M2Cn "dimensionless" 2,
+                     M2Apply M2Power [ sigma, M2Cn "dimensionless" 2 ]
+                   ]
+               ]
+            ]
+         ]
+     ]
+
+unToMMLASTPDF x (StudentTDistribution loc scale degf) =
+  let
+    t = M2Apply M2Divide [M2Apply M2Minus [x, M2Cn "dimensionless" loc],
+                          M2Cn "dimensionless" scale]
+    nu = M2Cn "dimensionless" (fromIntegral degf)
+    nuP1Half = M2Apply M2Divide [M2Apply M2Plus [nu, M2Cn "dimensionless" 1],
+                                 M2Cn "dimensionless" 2]
+  in
+   M2Apply M2Times [
+     M2Apply M2Divide [
+        mmlGammaFunc nuP1Half,
+        M2Apply M2Times [
+            M2Apply (M2Root Nothing) [
+               M2Apply M2Times [nu, M2Pi]
+                           ],
+            mmlGammaFunc (M2Apply M2Divide [nu, M2Cn "dimensionless" 2])
+          ]
+      ],
+     M2Apply M2Power [
+         M2Apply M2Plus [
+            M2Cn "dimensionless" 1,
+            M2Apply M2Divide [
+                M2Apply M2Power [x, M2Cn "dimensionless" 2],
+                nu
+              ]
+           ]
+       ],
+       M2Apply M2Minus [
+           M2Apply M2Divide [
+              M2Apply M2Plus [nu, M2Cn "dimensionless" 1],
+              M2Cn "dimensionless" 2
+            ]
+         ]
+     ]
+
+unToMMLASTPDF x (UniformDistribution min max) =
+  let
+    a = M2Cn "dimensionless" min
+    b = M2Cn "dimensionless" max
+  in
+   M2Piecewise [(M2Apply M2Divide [M2Cn "dimensionless" 1, M2Apply M2Minus [b, a]],
+                 M2Apply M2And [M2Apply M2Geq [x, a], M2Apply M2Leq [x, b]])] $ Just
+     (M2Cn "dimensionless" 0)
+
+unToMMLASTPDF x (MixtureModel components) =
+  M2Apply M2Plus $ flip map components $ \(weight, component) -> 
+    M2Apply M2Times [M2Cn "dimensionless" weight, unToMMLAST component]
+
+unToMMLASTPDF x (MultivariateNormalDistribution mean cov) = error "Multivariate normal distribution not yet implemented"
+
+unToMMLASTPDF x (MultivariateStudentTDistribution mean cov degf) = error "Multivariate student-T distribution not yet implemented"
+
+unToMMLASTPDF x (BetaDistribution alphaP betaP) =
+  let
+    alpha = M2Cn "dimensionless" alphaP
+    beta = M2Cn "dimensionless" betaP
+  in
+   M2Apply M2Divide [
+     M2Apply M2Times [
+        M2Apply M2Power [x, M2Apply M2Minus [alpha, M2Cn "dimensionless" 1]],
+        M2Apply M2Power [M2Apply M2Minus [M2Cn "dimensionless" 1, x],
+                         M2Apply M2Minus [beta, M2Cn "dimensionless" 1]]
+        ],
+     M2Apply (M2Int { m2intDegree = Nothing, m2intLowlimit = Just (M2Cn "dimensionless" 0), m2intUplimit = Just (M2Cn "dimensionless" 1), m2intBvar = "betaBvar" }) [
+     let
+       u = M2Ci "betaBvar"
+     in
+      M2Apply M2Times [
+        M2Apply M2Power [u, M2Apply M2Minus [alpha, M2Cn "dimensionless" 1]],
+        M2Apply M2Power [M2Apply M2Minus [M2Cn "dimensionless" 1, u], M2Apply M2Minus [beta, M2Cn "dimensionless" 1]]
+      ]]
+     ]
+
+unToMMLASTPDF x (LaplaceDistribution alpha beta) = undefined
+
+unToMMLASTPDF x (CauchyDistribution loc scale) = undefined
+
+unToMMLASTPDF x (WeibullDistribution loc scale) = undefined
+
+unToMMLASTPDF x (LogisticDistribution loc scale) = undefined
+
+unToMMLASTPDF x (ChiSquareDistribution degf) = undefined
+
+unToMMLASTPDF x (GeometricDistribution prob) = undefined
+
+unToMMLASTPDF x (HypergeometricDistribution nsuccess ntrials npop) = undefined
+
+unToMMLASTPDF x (FDistribution denom num) = undefined
+
+unToMMLASTPDF x (NegativeBinomialDistribution numFailures prob) = undefined
+
+unToMMLASTPDF x (ParetoDistribution scale shape) = undefined
+
+unToMMLASTPDF x (WishartDistribution degf scales) = undefined
+
+unToMMLASTPDF x (BernoulliDistribution prob) = undefined
+
+unToMMLASTPDF _ _ = error "PDF requested for unexpected UncertML distribution"
+
+unToMMLASTPMF k (PoissonDistribution poissonRate) =
+  let
+    lambda = M2Cn "dimensionless" poissonRate
+  in
+    M2Apply M2Times [
+      M2Apply M2Divide [M2Apply M2Power [lambda, k], M2Apply M2Factorial [k]],
+      M2Apply M2Exp [M2Apply M2Minus [lambda]]
+                    ]
+
+unToMMLASTPMF k (BinomialDistribution numTrials pSuccess) =
+  let
+    n = M2Cn "dimensionless" (fromIntegral numTrials)
+    p = M2Cn "dimensionless" pSuccess
+  in
+   M2Apply M2Times [
+     M2Apply M2Divide [
+        M2Apply M2Factorial [n],
+        M2Apply M2Times [M2Apply M2Factorial [k],
+                         M2Apply M2Factorial [M2Apply M2Minus [n, k]]]
+        ],
+     M2Apply M2Power [p, k],
+     M2Apply M2Power [M2Apply M2Minus [M2Cn "dimensionless" 1, p],
+                      M2Apply M2Minus [n, k]]
+                   ]
+   
+mmlGammaFunc z =
+  let
+    t = M2Ci "gammaBvar"
+  in
+   M2Apply (M2Int { m2intDegree = Nothing, m2intLowlimit = Just (M2Cn "dimensionless" 0), m2intUplimit = Just M2Infinity, m2intBvar = "gammaBvar" })
+     [M2Apply M2Times [M2Apply M2Power [t, M2Apply M2Minus [z, M2Cn "dimensionless" (-1)]],
+                       M2Apply M2Exp [M2Apply M2Minus [t]]]]
 
 arrowSum :: ArrowPlus a => [a b c] -> a b c
 arrowSum = foldl' (<+>) zeroArrow
