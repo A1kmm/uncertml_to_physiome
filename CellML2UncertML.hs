026b49b2193f1b95059266fa84fe24a18898cb32
Andrew Miller <ak.miller@auckland.ac.nz>
2012-01-10 12:52:44 +1300

    Add CellML2UncertML binary (compiles but not yet usable due key parts yet to be implemented)

--- CellML2UncertML.hs
+++ CellML2UncertML.hs
@@ -1,6 +1,8 @@
 import Text.XML.HXT.Core
 import MathML
 import UncertML
+import Control.Monad
+import Text.XML.HXT.Arrow.ParserInterface
 
 main = do
   liftM doConversion getContents >>= putStr
@@ -8,3 +10,6 @@ main = do
 doConversion :: String -> String
 doConversion = concat . runLA (xshow $ (\v -> (v, v)) ^>> parseXmlDoc >>> propagateNamespaces >>>
                                (hasQName (mkNsName "mml:mathml" mathmlNS) /> xmlToMathML2 >>> arr mmlToUnAST >>> unlistA >>> uncertmlASTToXML))
+
+mmlToUnAST = error "TODO: Implement mmlToUnAST"
+uncertmlASTToXML = error "TODO: Implement uncertmlASTToXML"
